13e45f165d2972fccbd54b9ad750c0fa818995e7
pha123661 <pha123661@gmail.com>
2022-04-22 15:04:14 +0800

    init

---
+++ HW2/main.py
@@ -0,0 +1,32 @@
+from flask import *
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return redirect("/index.html")
+
+
+@app.route("/index.html")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/sign-up.html")
+def sign_up():
+    return render_template("sign-up.html")
+
+
+@app.route("/nav.html")
+def nav():
+    return render_template("nav.html")
+
+
+def main():
+    app.run()
+
+
+if __name__ == '__main__':
+    main()
