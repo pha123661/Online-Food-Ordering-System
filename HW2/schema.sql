1dc332f84052ff9901a1acf157ffe81c86917d3f
Li Sheng-Wei <pha123661@gmail.com>
2022-05-09 02:45:54 +0800

    Optimize & format schema

--- HW2/schema.sql
+++ HW2/schema.sql
@@ -1,73 +1,88 @@
-create table if not exists Users(
-    UID INTEGER PRIMARY KEY AUTOINCREMENT,
-    U_account varchar(255) UNIQUE NOT NULL,
-    U_password varchar(255) NOT NULL,
-    U_name varchar(255) NOT NULL, -- {First name}  {Last name}
-    U_type int NOT NULL, -- 0: Normal user, 1: Shop owner
-    U_latitude float NOT NULL,
-    U_longitude float NOT NULL,
-    U_phone varchar(255) NOT NULL,
-    U_balance int NOT NULL
-);
+CREATE TABLE
+    if NOT EXISTS Users(
+        UID INTEGER PRIMARY KEY AUTOINCREMENT,
+        U_account VARCHAR(256) UNIQUE NOT NULL,
+        U_password VARCHAR(64) NOT NULL,
+        U_name VARCHAR(256) NOT NULL,
+        -- {First name}  {Last name}
+        U_type INT NOT NULL,
+        -- 0: Normal user, 1: Shop owner
+        U_latitude FLOAT NOT NULL,
+        U_longitude FLOAT NOT NULL,
+        U_phone VARCHAR(10) NOT NULL,
+        U_balance INT NOT NULL
+    );
 
-create table if not exists Stores(
-    SID INTEGER PRIMARY KEY AUTOINCREMENT,
-    S_name varchar(255) UNIQUE NOT NULL,
-    S_latitude float NOT NULL,
-    S_longitude float NOT NULL,
-    S_phone varchar(255) NOT NULL,
-    S_foodtype varchar(255) NOT NULL,
-    S_owner int NOT NULL,
-    foreign key (S_owner) references Users(UID)
-);
+CREATE TABLE
+    if NOT EXISTS Stores(
+        SID INTEGER PRIMARY KEY AUTOINCREMENT,
+        S_name VARCHAR(256) UNIQUE NOT NULL,
+        S_latitude FLOAT NOT NULL,
+        S_longitude FLOAT NOT NULL,
+        S_phone VARCHAR(10) NOT NULL,
+        S_foodtype VARCHAR(256) NOT NULL,
+        S_owner INT NOT NULL,
+        FOREIGN key (S_owner) REFERENCES Users(UID)
+    );
 
-create table if not exists Orders(
-    OID INTEGER PRIMARY KEY AUTOINCREMENT,
-    O_status int NOT NULL, -- 0: not done, 1: done, -1:canceled
-    O_start_time datetime NOT NULL, -- format: 'yyyy-mm-dd hh-mi-ss'
-    O_end_time datetime NOT NULL,
-    O_distance float NOT NULL,
-    O_amount int NOT NULL,
-    O_type int NOT NULL, -- 0: take-out, 1: delivery
-    SID int NOT NULL,
-    foreign key (SID) references Stores(SID)
-);
+CREATE TABLE
+    if NOT EXISTS Orders(
+        OID INTEGER PRIMARY KEY AUTOINCREMENT,
+        O_status INT NOT NULL,
+        -- 0: not done, 1: done, -1:canceled
+        O_start_time datetime NOT NULL,
+        -- format: 'yyyy-mm-dd hh-mi-ss'
+        O_end_time datetime NOT NULL,
+        O_distance FLOAT NOT NULL,
+        O_amount INT NOT NULL,
+        O_type INT NOT NULL,
+        -- 0: take-out, 1: delivery
+        SID INT NOT NULL,
+        FOREIGN key (SID) REFERENCES Stores(SID)
+    );
 
-create table if not exists Process_Order(
-    UID int NOT NULL,
-    OID int NOT NULL,
-    PO_type int NOT NULL, -- 0: User order, 1: User cancel, 2: Owner deliver, 3:Owner cancel
-    primary key (UID, OID),
-    foreign key (UID) references Users(UID),
-    foreign key (OID) references Orders(OID)
-);
+CREATE TABLE
+    if NOT EXISTS Process_Order(
+        UID INT NOT NULL,
+        OID INT NOT NULL,
+        PO_type INT NOT NULL,
+        -- 0: User order, 1: User cancel, 2: Owner deliver, 3:Owner cancel
+        PRIMARY key (UID, OID),
+        FOREIGN key (UID) REFERENCES Users(UID),
+        FOREIGN key (OID) REFERENCES Orders(OID)
+    );
 
-create table if not exists Transaction_Record(
-    TID INTEGER PRIMARY KEY AUTOINCREMENT,
-    T_action int NOT NULL, -- 0: deduct, 1: top-up
-    T_amount int NOT NULL,
-    UID int,
-    foreign key (UID) references Users(UID)
-);
+CREATE TABLE
+    if NOT EXISTS Transaction_Record(
+        TID INTEGER PRIMARY KEY AUTOINCREMENT,
+        T_action INT NOT NULL,
+        -- 0: deduct, 1: top-up
+        T_amount INT NOT NULL,
+        UID INT,
+        FOREIGN key (UID) REFERENCES Users(UID)
+    );
 
-create table if not exists Products(
-    PID INTEGER PRIMARY KEY AUTOINCREMENT,
-    P_name varchar(255) NOT NULL,
-    P_price int unsigned NOT NULL,
-    P_quantity int unsigned NOT NULL,
-    P_image BLOB NOT NULL, -- image encoded by base64
-    P_imagetype varchar(25) NOT NULL,
-    P_owner int NOT NULL,
-    P_store int NOT NULL,
-    foreign key (P_owner) references Users(UID),
-    foreign key (P_store) references Stores(SID)
-);
+CREATE TABLE
+    if NOT EXISTS Products(
+        PID INTEGER PRIMARY KEY AUTOINCREMENT,
+        P_name VARCHAR(256) NOT NULL,
+        P_price INT unsigned NOT NULL,
+        P_quantity INT unsigned NOT NULL,
+        P_image BLOB NOT NULL,
+        -- image encoded by base64
+        P_imagetype VARCHAR(25) NOT NULL,
+        P_owner INT NOT NULL,
+        P_store INT NOT NULL,
+        FOREIGN key (P_owner) REFERENCES Users(UID),
+        FOREIGN key (P_store) REFERENCES Stores(SID)
+    );
 
-create table if not exists O_Contains_P(
-    OID int NOT NULL,
-    PID int NOT NULL,
-    Quantity int unsigned NOT NULL,
-    primary key (OID, PID),
-    foreign key (OID) references Orders(OID),
-    foreign key (PID) references Products(PID)
-);
+CREATE TABLE
+    if NOT EXISTS O_Contains_P(
+        OID INT NOT NULL,
+        PID INT NOT NULL,
+        Quantity INT unsigned NOT NULL,
+        PRIMARY key (OID, PID),
+        FOREIGN key (OID) REFERENCES Orders(OID),
+        FOREIGN key (PID) REFERENCES Products(PID)
+    );
